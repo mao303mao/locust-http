a538d3434db3ba9cac8c4123adf10e33cacf7a5c
unknown <maohuan@focuschina.com>
2020-08-25 14:06:43 +0800

    added

---
+++ worker/boomerHazardServer.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	proto "locust_hazard/proto"
+	"locust_hazard/utils/boomerWrap"
+	"locust_hazard/utils/gRpcEtcd"
+	"log"
+	"math/rand"
+	"net"
+	"net/url"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/levigross/grequests"
+	"github.com/myzhan/boomer"
+	"go.etcd.io/etcd/v3/clientv3"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var host = gRpcEtcd.GetLocalNetIpV4() // 本机ip
+var ServiceName = "boomer_service"
+var (
+	Host        = flag.String("Host", host, "listening host")                           // 服务器Port
+	Port        = flag.Int("Port", 3000, "listening port")                           // 服务器Port
+	EtcdAddr    = flag.String("EtcdAddr", "127.0.0.1:2379", "register etcd address") // etcd的地址
+)
+
+// etcd客户端
+var cli *clientv3.Client
+// 全局boomer
+var globalBoomer *boomer.Boomer
+// 接受来自EndBoomer的请求处理
+var quitSignal chan int=make(chan int)
+// boomer运行状态
+var boomerStatus = false
+// boomer等待退出
+func waitForQuit(gboomer *boomer.Boomer) {
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	quitByClient :=false
+	boomer.Events.Subscribe("boomer:quit", func() {
+		defer func(){
+			r:=recover();if r!=nil{
+			fmt.Println("处理Boomer关闭遇到异常:",r)
+		}}()
+		boomerStatus=false // 结束运行
+		if !quitByClient{
+			fmt.Println("事件订阅中获取了非Client关闭Boomer的消息")
+			wg.Done()
+		}
+	})
+	go func(){ // 此处添加通过EndBommer获取强制关闭boomer信号处理的代码
+		<-quitSignal
+		fmt.Println("从管理机client获取了关闭Boomer的消息")
+		quitByClient=true
+		gboomer.Quit()
+		wg.Done()
+
+	}()
+	wg.Wait()
+}
+
+// rpc服务接口
+type BoomerCallService struct{}
+
+func (hes *BoomerCallService) InitBommer(ctx context.Context, req *proto.InitBommerRequest) (*proto.BoomerCallResponse, error) {
+	if boomerStatus{ // 如果运行，结束（暂时不加全局锁）
+		return &proto.BoomerCallResponse{
+			Status:  false,
+			Message: fmt.Sprintf("%s--当前boomer正在运行，不需要初始化", time.Now().Format("2006-01-02 15:04:05")),
+		}, nil
+	}
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	rand.Seed(time.Now().UnixNano()) // 每次初始化时决定随机
+	baseReqOptions := &grequests.RequestOptions{ // 基本的http请求配置
+		InsecureSkipVerify: true, // 忽略https证书错误
+		MaxIdleConnsPerHost: 2000, // 限制连接数
+		DisableKeepAlives:   false,
+		RedirectLimit:       -1, // 不进行重定向
+	}
+	if req.GetHttpProxy()!=""{
+		httpProxyUrl, err := url.Parse(fmt.Sprintf("http://%s",req.GetHttpProxy()))
+		if err!=nil{
+			return &proto.BoomerCallResponse{
+				Status:  false,
+				Message: fmt.Sprintf("%s--代理设置有误，请检查", time.Now().Format("2006-01-02 15:04:05")),
+			}, nil
+		}
+		httpsProxyUrl, _ := url.Parse(fmt.Sprintf("http://%s",req.GetHttpProxy()))
+		baseReqOptions.Proxies=map[string]*url.URL{
+			"http":  httpProxyUrl,
+			"https": httpsProxyUrl,
+		}
+	}
+	storedParamValues := map[string]string{} // 全局变量存储器
+	var reqSession *grequests.Session // 全局http session
+	if req.IsSession {
+		reqSession = grequests.NewSession(baseReqOptions)
+	} else {
+		reqSession = nil
+	}
+	if req.GetPreTask() != nil {  // 执行前置任务，主要用于更新session及存储一些全局量，比如一些登录相关的token
+		err:=boomerWrap.DoPreTask(baseReqOptions, reqSession, req.GetPreTask(), storedParamValues)
+		if err!=nil{
+			return &proto.BoomerCallResponse{
+				Status:  false,
+				Message: fmt.Sprintf("%s--初始化Boomer失败，前置步骤执行失败[%s]",
+					time.Now().Format("2006-01-02 15:04:05"),err.Error()),
+			}, nil
+		}
+	}
+
+	taskList := []*boomer.Task{}
+	// fmt.Println(req.GetLocustMaster())
+	globalBoomer = boomer.NewBoomer(req.GetLocustMaster(), 5557)
+	for _, testTask := range req.MainTask { // 创建boomer任务
+		task := &boomer.Task{
+			Name:   testTask.GetTaskName(),
+			Weight: int(testTask.GetTaskWeight()),
+			Fn:     boomerWrap.MakeTestTask(baseReqOptions, reqSession, testTask, globalBoomer, storedParamValues),
+		}
+		fmt.Println(*task)
+		taskList = append(taskList, task)
+	}
+	go func() { // 开启一个协程等待结束
+		globalBoomer.Run(taskList...)
+		boomerStatus = true // boomer正在运行
+		waitForQuit(globalBoomer)
+		log.Println("boomer is shut down............")
+	}()
+	return &proto.BoomerCallResponse{
+		Status:  true,
+		Message: fmt.Sprintf("%s--已经成功获取初始化Boomer信息", time.Now().Format("2006-01-02 15:04:05")),
+	}, nil
+}
+
+func (hes *BoomerCallService) EndBommer(ctx context.Context, req *proto.EndBommerRequest) (*proto.BoomerCallResponse, error) {
+	if !boomerStatus{ // 如果非运行，结束（暂时不加全局锁）
+		return &proto.BoomerCallResponse{
+			Status:  false,
+			Message: fmt.Sprintf("%s--当前boomer没有运行，不需要关闭", time.Now().Format("2006-01-02 15:04:05")),
+		}, nil
+	}
+	quitSignal<-1 // 写入关闭信号
+	// 此处添加关闭Boomer的信号处理
+	return &proto.BoomerCallResponse{
+		Status:  true,
+		Message: fmt.Sprintf("%s--已经成功获取关闭Boomer信息", time.Now().Format("2006-01-02 15:04:05")),
+	}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	// 监听网络
+	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *Port))
+	if err != nil {
+		fmt.Println("监听网络失败：", err)
+		return
+	}
+	defer listener.Close()
+
+	// 创建grpc句柄
+	srv := grpc.NewServer()
+	defer srv.GracefulStop()
+
+	// 将Server结构体注册到grpc服务中
+	proto.RegisterBoomerCallServiceServer(srv, &BoomerCallService{})
+
+	// 将服务地址注册到etcd中
+
+	cli, err = gRpcEtcd.NewEtcdClient(*EtcdAddr)
+	if err != nil {
+		fmt.Println("创建ETCD客户端失败：", err)
+		return
+	}
+	// println(fmt.Sprintf("%n"),cli.ActiveConnection())
+
+	serverAddr := fmt.Sprintf("%s:%d", *Host, *Port)
+	fmt.Printf("服务地址: %s\n", serverAddr)
+	gRpcEtcd.Register(cli, ServiceName, serverAddr, 5)
+
+	// 关闭信号处理
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGHUP, syscall.SIGQUIT)
+	go func() {
+		s := <-ch
+		gRpcEtcd.UnRegister(cli, ServiceName, serverAddr)
+		if i, ok := s.(syscall.Signal); ok {
+			os.Exit(int(i))
+		} else {
+			os.Exit(0)
+		}
+	}()
+
+	// 监听服务
+	err = srv.Serve(listener)
+	if err != nil {
+		fmt.Println("gRPC服务的监听异常：", err)
+		return
+	}
+}
---
+++ worker/utils/gRpcEtcd/publicMod.go
@@ -0,0 +1,141 @@
+package gRpcEtcd
+// 此处选摘“疯一样的狼人”的文档
+// https://www.cnblogs.com/wujuntian/p/12838041.html
+import (
+	"go.etcd.io/etcd/v3/clientv3"
+	"strings"
+	"time"
+	"fmt"
+	"net"
+	"golang.org/x/net/context"
+	"go.etcd.io/etcd/v3/clientv3/concurrency"
+)
+const schema = "ns"
+
+// 创建Etcd客户端
+func NewEtcdClient(etcdAddr string) (*clientv3.Client,error){
+	return clientv3.New(clientv3.Config{
+		Endpoints:   strings.Split(etcdAddr, ";"),
+		DialTimeout: 5 * time.Second,
+	})
+
+}
+
+func GetLocalNetIpV4() string{
+	addrs, err := net.InterfaceAddrs()
+	if err != nil{
+		fmt.Println(err)
+		return "127.0.0.1"
+	}
+	for _, value := range addrs{
+		if ipnet, ok := value.(*net.IPNet); ok && !ipnet.IP.IsLoopback(){
+			if ipnet.IP.To4() != nil{
+				if strings.Index(ipnet.IP.String(),"192.168.")==0{
+					return ipnet.IP.String()
+				}
+			}
+		}
+	}
+	return "127.0.0.1"
+}
+
+// 将服务地址注册到etcd中
+func Register(cli *clientv3.Client ,serviceName,serverAddr string, ttl int64) error {
+
+	// 进行心跳检测
+	ticker := time.NewTicker(time.Second * time.Duration(ttl))
+	go func() {
+		key := "/" + schema + "/" + serviceName + "/" + serverAddr
+		fmt.Println(key)
+		for {
+			resp, err := cli.Get(context.Background(), key)
+			// fmt.Printf("resp:%+v\n", resp)
+			if err != nil {
+				fmt.Printf("获取服务地址失败：%s\n", err)
+			} else if resp.Count == 0 { // 未注册
+				err = KeepAlive(cli,serviceName, serverAddr, ttl)
+				if err != nil {
+					fmt.Printf("保持连接失败：%s\n", err)
+				}
+			}
+			<-ticker.C
+		}
+	}()
+
+	return nil
+}
+
+// 保持服务器与etcd的长连接
+func KeepAlive(cli *clientv3.Client,serviceName, serverAddr string, ttl int64) error {
+	// 创建租约
+	leaseResp, err := cli.Grant(context.Background(), ttl)
+	if err != nil {
+		fmt.Printf("创建租期失败：%s\n", err)
+		return err
+	}
+
+	// 将服务地址注册到etcd中
+	key := "/" + schema + "/" + serviceName + "/" + serverAddr
+	_, err = cli.Put(context.Background(), key, serverAddr, clientv3.WithLease(leaseResp.ID))
+	if err != nil {
+		fmt.Printf("注册服务失败：%s\n", err)
+		return err
+	}
+
+	// 建立长连接
+	ch, err := cli.KeepAlive(context.Background(), leaseResp.ID)
+	if err != nil {
+		fmt.Printf("建立长连接失败：%s\n", err)
+		return err
+	}
+
+	// 清空keepAlive返回的channel
+	go func() {
+		for {
+			<-ch
+		}
+	}()
+	return nil
+}
+
+// 取消注册
+func UnRegister(cli *clientv3.Client,serviceName, serverAddr string) {
+	if cli != nil {
+		key := "/" + schema + "/" + serviceName + "/" + serverAddr
+		cli.Delete(context.Background(), key)
+		cli.Close()
+	}
+}
+
+// 获取分布式锁，本应用用不到
+func NewTaskMutex(cli *clientv3.Client,serviceName string) (*concurrency.Session,*concurrency.Mutex,error){
+	prefix := "/myLock/" + serviceName
+	mutexSession,err:=concurrency.NewSession(cli)
+	if err!=nil{
+		return nil,nil,err
+	}
+	return mutexSession, concurrency.NewMutex(mutexSession,prefix),nil
+}
+
+// 获取key值
+func GetKey(cli *clientv3.Client,keyName string)[]map[string]string{
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	res, err := cli.Get(ctx, keyName)
+	cancelFunc()
+	if err != nil {
+		return nil
+	}
+    var kvMap []map[string]string
+	for k, v := range res.Kvs {
+		fmt.Println("获取到Kvs的值", k, string(v.Key), string(v.Value))
+		kvMap=append(kvMap,map[string]string{"key":string(v.Key),"value":string(v.Value)})
+	}
+	return kvMap
+}
+// 设置key值
+func SetKey(cli *clientv3.Client,keyName string,keyValue string) error{
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	_, err := cli.Put(ctx, keyName,keyValue)
+	cancelFunc()
+	return err
+}
